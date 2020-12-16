405a18a165c9613047c955327d281dfa1f3eea3c
Furkan <furkan_demir00@hotmail.com>
2020-12-16 17:50:36 +0300

    S

--- RISE/src/UI/Board.java
+++ RISE/src/UI/Board.java
@@ -9,19 +9,21 @@ import java.io.IOException;
 public class Board{
 
     String currentDir;
-   public Board(){
+    public Board(){
 
-   }
+    }
 
-   public Pane show(GamePlay a) throws IOException {
+    public Pane show(GamePlay a) throws IOException {
        FXMLLoader loader = new FXMLLoader(getClass().getResource("/UI/Map.fxml"));
        Pane root = (Pane) loader.load();
        return root;
-   }
-   public void clicked(Event e){
+    }
+    public void clicked(Event e){
        System.out.print("001");
    }
     public void clicke(Event e){
         System.out.print("002");
     }
+
+
 }
